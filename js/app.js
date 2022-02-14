3ddbd117c4e64ddee51e706e7056893abb87e49c
_0M3G4_ <58949193+RayanBoughalia@users.noreply.github.com>
2022-02-14 17:41:38 +0100

    hello

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const button = document.querySelector('#button');
 
 button.addEventListener("click", () => {
     document.querySelector('#button').innerHTML = prompt('button name\'s: ');
-    if (button.innerHTML == 'hide') {
-        button.hidden = true
-    }
 }, false)
+
+if (button.innerHTML == 'hide') {
+    button.hidden = true
+}
