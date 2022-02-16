68dd3cf67b53ecdb8a71122290def9ba3d1d11f7
_0M3G4_ <58949193+RayanBoughalia@users.noreply.github.com>
2022-02-16 14:52:47 +0100

    goodbye

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 const button = document.querySelector('#button');
 
+class Local_Storage{
+    get ADD(key, value){
+        localStorage.setItem(key, value);
+    }
+
+    get REMOVE(key){
+        localStorage.removeItem(key)
+    }
+
+    get CLEAR(){
+        localStorage.clear()
+    }
+}
+
 button.addEventListener("click", () => {
     var name = prompt('button name\'s: ');
-    localStorage.setItem('name', name);
     document.querySelector('#button').innerHTML = name; 
     if (name == "hide") {
         button.hidden = true
