bff2e480307995001f6875a39cff53f69bc75a15
_0M3G4_ <58949193+RayanBoughalia@users.noreply.github.com>
2022-02-16 14:37:37 +0100

    je

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const button = document.querySelector('#button');
 button.addEventListener("click", () => {
     var name = prompt('button name\'s: ');
     document.querySelector('#button').innerHTML = name; 
-    localStorage.setItem('RecentName', name)
     if (name == "hide") {
         button.hidden = true
     }
