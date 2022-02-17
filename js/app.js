d25e2c519c040931d9d4a12a0c6fd543fc565dbf
_0M3G4_ <58949193+RayanBoughalia@users.noreply.github.com>
2022-02-17 20:32:43 +0100

    changes made

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('#button');
+const button = document.querySelector('#button1');
 
 class Local_Storage{
     add(key, value){
@@ -20,10 +20,14 @@ const local_storage = new Local_Storage();
 button.addEventListener("click", () => {
     var name = prompt('button name\'s: ');
     local_storage.add('nom', name)
-    document.querySelector('#button').innerText = name; 
+    document.querySelector('#button1').innerText = name; 
     if (name == "hide") {
         button.hidden = true
     }
+}, false)
+
+document.addEventListener('keypress', e => {
+    console.log(e.key)
 }, false)
 
 
